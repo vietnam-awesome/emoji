064cb825d8b7503fdbc1595743a707abb9f92c46
David Nguyen <h250694@gmail.com>
2026-09-16 14:47:32 +0700

    refactor(ui): remove obsolete homepage search button wrapper

--- src/components/beui/HomeSearchButton.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from '../motion/button';
-
-export default function HomeSearchButton() {
-  return (
-    <Button type="submit" size="lg" className="beui-search-button">
-      Search
-    </Button>
-  );
-}
